e3a05d07c52dacee4242ca200bfc4dae4c05bfaf
Herrian <javierazuara@me.com>
2013-06-28 18:49:02 -0400

    Initial Commit

---
+++ JASlideNavigationBar/JAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JAAppDelegate.h
+//  JASlideNavigationBar
+//
+//  Created by Javier Daniel Peralta Azuara on 6/28/13.
+//  Copyright (c) 2013 Javier Daniel Peralta Azuara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JASlideNavigationBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JASlideNavigationBar
+//
+//  Created by Javier Daniel Peralta Azuara on 6/28/13.
+//  Copyright (c) 2013 Javier Daniel Peralta Azuara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAAppDelegate class]));
+    }
+}
